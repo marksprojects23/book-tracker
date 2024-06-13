09ab33282bbcf579f2e9b0cdce6f542b6e83229c
marksprojects23 <markc7856@yahoo.com>
2024-06-12 18:02:37 -0700

    finally added all comments

--- client/src/components/DeleteBook.js
+++ client/src/components/DeleteBook.js
@@ -1,22 +1,21 @@
 // client/src/components/DeleteBook.js
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState } from 'react';        // Imports React as well as states.
+import axios from 'axios';                      // Imports axios for HTTP requests.
 
-const DeleteBook = () => {
-  const [id, setId] = useState('');
+const DeleteBook = () => {                      // Let's define how to delete books.
+  const [id, setId] = useState('');             // We will use the inputted book ID as the state.
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    await axios.delete(`http://localhost:5000/books/delete/${id}`);
-    // Handle success (e.g., clear form, show success message)
-  };
+  const handleSubmit = async (e) => {           // When the Delete Book button is pressed,
+    e.preventDefault();                         // the page is kept from refreshing,
+    await axios.delete(`http://localhost:5000/books/delete/${id}`); // and then we send the DELETE request
+  };                                            // .
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <input type="text" value={id} onChange={(e) => setId(e.target.value)} placeholder="Book ID" />
-      <button type="submit">Delete Book</button>
-    </form>
+  return (                                      // JSX.
+    <form onSubmit={handleSubmit}>              {/*Upon submission of the form, handleSubmit() is called.*/}
+      <input type="text" value={id} onChange={(e) => setId(e.target.value)} placeholder="Book ID" />{/*The state is changed to whatever the inputted ID is, otherwise it's "Book ID".*/}
+      <button type="submit">Delete Book</button>{/*The button will be labeled Delete Book.*/}
+    </form>                                     // .
   );
 };
 
-export default DeleteBook;
+export default DeleteBook;                      // Export this component.
